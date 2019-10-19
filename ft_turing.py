f9d65ad6929fd9e2dc620da0cb6d8222e23983fe
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2019-10-19 22:10:24 +0200

    feature: implement first version palindrome, minor changes to turing machine

--- ft_turing.py
+++ ft_turing.py
@@ -21,30 +21,36 @@ parser.add_argument('input', help='input of the machine')
 args = parser.parse_args()
 
 machine = json.load(args.jsonfile)
-machineInput = args.input
 
 # TODO: Add validation for the actual input
 
 print('Running machine description: ' + machine['name'])
 
 # initial machine state values
-tape = list(machineInput)
-tape.append(machine['blank'] * 5)
+tape = list(args.input)
+tape.extend(list(machine['blank'] * 1))
 state = machine['initial']
 pos = 0
 
 # Function to print current state of tape
 def print_tape():
     tape_print = tape.copy()
-    tape_print[pos] = bcolors.UNDERLINE + tape_print[pos] + bcolors.ENDC
+    tape_print[pos] = bcolors.BOLD + bcolors.UNDERLINE + bcolors.WARNING + tape_print[pos] + bcolors.ENDC
     print(f"[{''.join(tape_print)}] ({state}, {tape[pos]})", end='')
+
+# Read the current pos of the tape
+def read_tape_block():
+    if len(tape) >= pos + 1:
+        return str(tape[pos])
+    else:
+        return str(machine['blank'])
 
 # State machine
 while state not in machine['finals']:
     transition = False
     # Finding the correct transition for the state and current tape value 
     for item in machine['transitions'][state]:
-        if str(item['read']) != str(tape[pos]):
+        if str(item['read']) != read_tape_block():
             continue
         else:
             transition = item
@@ -52,19 +58,19 @@ while state not in machine['finals']:
 
     # Check if condition exists
     if transition == False:
-        print(f"No transition found for {str(tape[pos])} in {state}")
-        exit()
+        raise LookupError(f"No transition found for {str(read_tape_block())} in {state}")
 
     print_tape()
+    print(f" -> ({transition['to_state']}, {transition['write']}, {transition['action']})")
     state = transition['to_state']
     tape[pos] = transition['write']
-    print(f" -> ({state}, {tape[pos]}, {transition['action']})")
+    
     if transition['action'] == 'RIGHT':
         pos += 1
     elif transition['action'] == 'LEFT':
         pos -= 1
     else:
-        print('Something unexpected happened, this action does not exist')
-        exit()
+        raise ValueError(f"Transition action '{transition['action']}' is not supported.")
 
+tape.pop()
 print('\n -- Result: ' + ''.join(tape))
